cf767c25ab1d190d0982a7e1938328ee32926a53
Charidimos Tzedakis <harris.jedakis@gmail.com>
2018-12-02 12:16:08 +0200

    uuid RFC4122 v1

--- api/routes/company/index.js
+++ api/routes/company/index.js
@@ -1,18 +1,26 @@
 'use strict';
 
 var router = global.express.Router();
+var uuidv1 = require('uuid/v1');
 var mongoose = require('mongoose');
 var Company = require('../../models/company');
 
 
 // ** create a new company entry */
 router.post('/', function handle(req, res) {
-  var test;
-  Company.create();
+  var displayName = req.displayName;
+  var name = displayName.toLowerCase();
+  var workspaces = req.workspaces;
   //* RFC4122 version 1 UUID
-  var NewId = new mongoose.mongo.ObjectId('56cb91bdc3464f14678934ca');
+  var newId = new mongoose.mongo.ObjectId(uuidv1());
+  var  companyDocument = {
+    _id: newId,
+    displayName,
+    name,
+    workspaces
+  };
 
-  Company.create(listDocuments, function result(err, results) {
+  Company.create(companyDocument, function result(err, results) {
     res.send(results);
     if (err) {
       res.send('Hello World!');
