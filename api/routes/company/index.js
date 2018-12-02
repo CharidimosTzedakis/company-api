5635ad459a6b561f4529e8c18f86b0abd5e3b8aa
Charidimos Tzedakis <harris.jedakis@gmail.com>
2018-12-02 04:19:29 +0200

    work on creating company obj

--- api/routes/company/index.js
+++ api/routes/company/index.js
@@ -8,13 +8,26 @@ var Company = require('../../models/company');
 // ** create a new company entry */
 router.post('/', function handle(req, res) {
   var test;
+  Company.create();
+  //* RFC4122 version 1 UUID
+  var NewId = new mongoose.mongo.ObjectId('56cb91bdc3464f14678934ca');
+
+  Company.create(listDocuments, function result(err, results) {
+    res.send(results);
+    if (err) {
+      res.send('Hello World!');
+    }
+  });
+});
+
+
+// ** update an existing company */
+router.patch('/', function handle(req, res) {
+  var test;
   // DB.newCompany();
   //* RFC4122 version 1 UUID
   var NewId = new mongoose.mongo.ObjectId('56cb91bdc3464f14678934ca');
   res.send('Hello World!');
 });
-
-
-// ** update an existing company */
 
 module.exports = router;
