e88b041dd7a65b124543a745870acab3df8b7526
Charidimos Tzedakis <harris.jedakis@gmail.com>
2018-12-04 09:55:34 +0200

    finished removing a user

--- api/routes/user/index.js
+++ api/routes/user/index.js
@@ -49,9 +49,7 @@ router.post('/:companyName', function handle(req, res) {
         return;
       }
 
-      if (workspaceToAddUser) {
-        workspaceToAddUser.users.push(newUser);
-      }
+      workspaceToAddUser.users.push(newUser);
 
       company.save(function onSave(saveErr, updatedCompany) {
         if (saveErr) res.status(400).send({ error: saveErr });
@@ -78,7 +76,49 @@ router.delete('/:companyName', function handle(req, res) {
   }
 
   var companyName = req.params.companyName;
+  Company.findOne({name: companyName}, function findResult(findErr, company) {
+    if (findErr) {
+      winston.info('POST /api/user: Error while fetching company from DB ' + findErr);
+      res.status(500).send('Error while updating.');
+    } else if (company) {
+      var userEmail = req.body.userEmail;
+      var workspaceName = req.body.workspaceName;
 
+      var workspaceToRemoveUser = company.workspaces.find(function find(w) {
+        if (w.name === workspaceName) return true;
+        return false;
+      });
+      if (!workspaceToRemoveUser) {
+        res.status(400).send('This workspace does not exist for this company.');
+        return;
+      }
+
+      //* check if user exists
+      var userExists = false;
+      var userToRemoveIndex = -1;
+      workspaceToRemoveUser.users.forEach(function forEachUser(u, i) {
+        if  (u.email === userEmail) {
+          userExists = true;
+          userToRemoveIndex = i;
+        }
+      });
+      if (!userExists) {
+        res.status(400).send('User does not exist.');
+        return;
+      }
+
+      workspaceToRemoveUser.users.splice(userToRemoveIndex, 1);
+
+      company.save(function onSave(saveErr, updatedCompany) {
+        if (saveErr) res.status(400).send({ error: saveErr });
+        winston.info('PATCH /api/company: Sucessfully updated: ' + updatedCompany);
+        res.send();
+      });
+    } else {
+      winston.info('PATCH /api/company: Company not found with id: ' + companyId);
+      res.status(404).send('PATCH /api/company: company not found.');
+    }
+  });
 });
 
 module.exports = router;
